47fd6c5a6168a05199975385603a05a8012f8b9a
Joachim LeBlanc <demosdemon@gmail.com>
2012-05-13 15:28:14 -0500

    fixed docset generation

--- generatedocs.sh
+++ generatedocs.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
+TEMP_DIR=/tmp
+
 /Applications/Doxygen.app/Contents/Resources/doxygen doxygen.config
-make --silent -C docs/html
+make --silent -C docs/html install
 make --silent -C docs/latex
+
+rm -f $TEMP_DIR/loadDocSet.scpt
+echo "tell application \"Xcode\"" >> $TEMP_DIR/loadDocSet.scpt
+echo "load documentation set with path \"/Users/$USER/Library/Developer/Shared/Documentation/DocSets/org.musicbrainz.libmusicbrainz-objc.docset\"" >> $TEMP_DIR/loadDocSet.scpt
+echo "end tell" >> $TEMP_DIR/loadDocSet.scpt
+osascript $TEMP_DIR/loadDocSet.scpt
+
 exit 0
