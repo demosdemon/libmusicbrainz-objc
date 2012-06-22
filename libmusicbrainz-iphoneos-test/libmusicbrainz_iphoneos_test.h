0c19a633a74489c8d540a74ebdafa106437a145e
Joachim LeBlanc <demosdemon@gmail.com>
2012-06-22 17:41:36 -0500

    Add in GTM, and start working on unittests

---
+++ libmusicbrainz-iphoneos-test/libmusicbrainz_iphoneos_test.h
@@ -0,0 +1,13 @@
+//
+//  libmusicbrainz_iphoneos_test.h
+//  libmusicbrainz-iphoneos-test
+//
+//  Created by Joachim LeBlanc on 6/21/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface libmusicbrainz_iphoneos_test : SenTestCase
+
+@end
