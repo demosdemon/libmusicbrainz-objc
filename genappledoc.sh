166e20263fc3bea49d35b93008e5884cf62b8ab0
Joachim LeBlanc <demosdemon@gmail.com>
2012-06-27 16:33:05 -0500

    tab to space

--- genappledoc.sh
+++ genappledoc.sh
@@ -6,28 +6,27 @@ VERSION=`git rev-parse --verify master`
 PUBLISH_URL="http://demosdemon.github.com/libmusicbrainz-objc/$OUTPUT/publish"
 
 /usr/local/bin/appledoc \
-	--create-html \
-	--create-docset \
-	--project-name "libmusicbrainz-objc" \
-	--project-company "MetaBrainz" \
-	--project-version "$VERSION" \
-	--company-id "org.metabrainz" \
-	--output "$DIRECTORY/$OUTPUT" \
-	--keep-undocumented-objects \
-	--keep-undocumented-members \
-	--search-undocumented-doc \
-	--docset-feed-url "$PUBLISH_URL/%DOCSETATOMFILENAME" \
-	--docset-package-url "$PUBLISH_URL/%DOCSETPACKAGEFILENAME" \
-	--publish-docset \
-	--keep-intermediate-files \
-	--ignore "docs" \
-	--ignore "submodules" \
+  --create-html \
+  --create-docset \
+  --project-name "libmusicbrainz-objc" \
+  --project-company "MetaBrainz" \
+  --project-version "$VERSION" \
+  --company-id "org.metabrainz" \
+  --output "$DIRECTORY/$OUTPUT" \
+  --keep-undocumented-objects \
+  --keep-undocumented-members \
+  --search-undocumented-doc \
+  --docset-feed-url "$PUBLISH_URL/%DOCSETATOMFILENAME" \
+  --docset-package-url "$PUBLISH_URL/%DOCSETPACKAGEFILENAME" \
+  --publish-docset \
+  --keep-intermediate-files \
+  --ignore "docs" \
+  --ignore "submodules" \
   --ignore ".git" \
   --ignore "DerivedData" \
   --ignore "build" \
-  --ignore ".m" \
-	--logformat "xcode" \
+  --logformat "xcode" \
   --verbose 2 \
   --no-warn-unknown-directive \
   --no-warn-invalid-crossref\
-	"$DIRECTORY"
+  "$DIRECTORY"
